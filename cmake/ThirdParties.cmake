e3152079db658fbba738cc8f6a78d8958957c5bd
data-man <dataman@tutanota.com>
2021-07-08 16:01:41 +0500

    Update CI

--- cmake/ThirdParties.cmake
+++ cmake/ThirdParties.cmake
@@ -2,16 +2,17 @@ include(CPM)
 
 CPMAddPackage(
   NAME fmt
-  URL https://github.com/fmtlib/fmt/archive/master.zip
-  VERSION master
-#  DOWNLOAD_ONLY YES
+  VERSION 20e4ef8b4c7ce67e880f28084af02fb9d1ce99c2
+  URL https://github.com/fmtlib/fmt/archive/20e4ef8b4c7ce67e880f28084af02fb9d1ce99c2.zip
+  URL_HASH SHA256=cc3e6064ebd56d432ba5afd74dc038dbb814e3740be476d5ca1620b6872e206a
 )
 
-CPMAddPackage(
-  NAME catch2
-  URL https://github.com/catchorg/Catch2/archive/devel.zip
-  VERSION devel
-#  DOWNLOAD_ONLY YES
-)
-
+if(LIBUNICODE_TESTING)
+  CPMAddPackage(
+    NAME catch2
+    VERSION 6f21a3609cea360846a0ca93be55877cca14c86d
+    URL https://github.com/catchorg/Catch2/archive/6f21a3609cea360846a0ca93be55877cca14c86d.zip
+    URL_HASH SHA256=8aec86bc5ccfd963caf3dc4cd137001a7f80bf54d44c69b187cc92b40851399a
+  )
+endif()
 
