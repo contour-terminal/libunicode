be9f207bd736f2a4a8537c56b71f3757a4e85f17
data-man <dataman@tutanota.com>
2021-07-22 17:03:57 +0500

    Update fmt library to latest

--- cmake/ThirdParties.cmake
+++ cmake/ThirdParties.cmake
@@ -1,13 +1,13 @@
 include(CPM)
 
 set(3rdparty_catch2_version "6f21a3609cea360846a0ca93be55877cca14c86d" CACHE STRING "catch2: commit hash")
-set(3rdparty_fmt_version "8465869d7b04ae00f657332b3374ecf95b5e49c0" CACHE STRING "fmt: commit hash")
+set(3rdparty_fmt_version "561834650aa77ba37b15f7e5c9d5726be5127df9" CACHE STRING "fmt: commit hash")
 
 CPMAddPackage(
   NAME fmt
   VERSION ${3rdparty_fmt_version}
   URL https://github.com/fmtlib/fmt/archive/${3rdparty_fmt_version}.zip
-  URL_HASH SHA256=3c0a45ee3e3688b407b4243e38768f346e75ec4a9b16cefbebf17252048395da
+  URL_HASH SHA256=310ba642b8944ecfc798fea39bfe66b91fd3c649d29c4fdfc218b0b2bb6c23d7
 )
 
 if(LIBUNICODE_TESTING)
