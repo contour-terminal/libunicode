ff5776634cfb2b7504b5970a09c81b40a3a00d80
data-man <dataman@tutanota.com>
2021-10-17 08:33:08 +0500

    Update fmtlib to 3e27455546:   - internal implementation of strftime   - improved floats formatting

--- cmake/ThirdParties.cmake
+++ cmake/ThirdParties.cmake
@@ -1,13 +1,13 @@
 include(CPM)
 
 set(3rdparty_catch2_version "6f21a3609cea360846a0ca93be55877cca14c86d" CACHE STRING "catch2: commit hash")
-set(3rdparty_fmt_version "561834650aa77ba37b15f7e5c9d5726be5127df9" CACHE STRING "fmt: commit hash")
+set(3rdparty_fmt_version "7a604cdd98eff2883b9dbd7d1ca1ddd8c147b80e" CACHE STRING "fmt: commit hash")
 
 CPMAddPackage(
   NAME fmt
   VERSION ${3rdparty_fmt_version}
   URL https://github.com/fmtlib/fmt/archive/${3rdparty_fmt_version}.zip
-  URL_HASH SHA256=310ba642b8944ecfc798fea39bfe66b91fd3c649d29c4fdfc218b0b2bb6c23d7
+  URL_HASH SHA256=3e2745554689c34fe23b6d5575c7ee0c74d2e2b83da15e2326263f6fec8ed409
 )
 
 if(LIBUNICODE_TESTING)
