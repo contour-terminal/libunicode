e05df8eb27eb8b8ec5c06b07c21fa33ed660e7d9
Christian Parpart <christian@parpart.family>
2026-02-16 12:10:32 +0100

    build: check in pre-generated tablegen output for Emscripten
    These files are normally generated by unicode_tablegen at build time,
    but Emscripten builds skip tablegen (WASM memory limits). Force-tracked
    despite .gitignore so CPM-fetched sources include them.
    
    Signed-off-by: Christian Parpart <christian@parpart.family>

---
+++ src/libunicode/codepoint_properties_data.h
@@ -0,0 +1,19 @@
+// This file was auto-generated using /home/christianparpart/projects/libunicode/src/libunicode/tablegen.cpp.
+#pragma once
+
+#include <libunicode/codepoint_properties.h>
+
+#include <array>
+#include <cstdint>
+
+namespace unicode::precompiled
+{
+
+extern std::array<uint8_t, 4352> const stage1;
+extern std::array<uint16_t, 54016> const stage2;
+extern std::array<codepoint_properties, 1410> const properties;
+extern std::array<uint8_t, 4352> const names_stage1;
+extern std::array<uint16_t, 59648> const names_stage2;
+extern std::array<std::string_view, 48954> const names_stage3;
+
+} // end namespace unicode::precompiled
